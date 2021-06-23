2e69ba0d38d1303a335996e2e31de1e822e31cb6
Agin-DropDisco <76779267+Agin-DropDisco@users.noreply.github.com>
2021-06-23 10:58:33 +0700

    Rebuild

--- contracts/DexSwap.sol
+++
@@ -1,9 +0,0 @@
-pragma solidity =0.5.16;
-
-import './DexSwapERC20.sol';
-
-contract DexSwap is DexSwapERC20 {
-    constructor(uint _totalSupply) public {
-        _mint(msg.sender, _totalSupply);
-    }
-}
